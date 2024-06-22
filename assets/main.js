22f922611e7b762cd02867d903d6819e7e3bdea8
sarco3t <14gainward88@gmail.com>
2024-06-22 17:11:55 +0300

    assets optimization

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+---
+layout: null
+---
+
+{% include_relative js/vendor/bootstrap.bundle.min.js %}
+{% include_relative js/vendor/jquery.min.js %}
+{% include_relative js/vendor/swiper-bundle.min.js %}
+{% include_relative js/vendor/isotope.pkgd.min.js %}
+{% include_relative js/vendor/fslightbox.js %}
+
+{% include_relative js/script.js %}
+{% include_relative js/swiper-script.js %}
+{% include_relative js/submit-form.js %}
